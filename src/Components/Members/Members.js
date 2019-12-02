7b3f3be4561b38aca84abfc4e933835ae1a72de0
rezan83 <rezan.moh83@gmail.com>
2019-12-02 19:41:03 +0100

    search functionality for members

--- src/Components/Members/Members.js
+++ src/Components/Members/Members.js
@@ -7,16 +7,33 @@ import uuid from "react-uuid";
 class Members extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { personData: Data };
+    this.state = { 
+      personData: Data,
+      query: ""
+    };
   }
   render() {
-    const AllMembers = this.state.personData.map(MyData => (
+    const AllMembers = this.state.personData
+    .filter(item =>(item.firstName+item.lastName).toLowerCase().search(this.state.query)!==-1)
+    .map(MyData => (
       <MembersList key={uuid()} personData={MyData} />
     ));
     return (
 
       <section className="membersContainer">
+      
+        {/* search */}
+        <div className="search-members" >
+          <label htmlFor="search">search</label>
+          <input id="search" type="text" 
+            value={this.state.query} 
+            onChange={(e) => this.setState({query: e.target.value})}
+          />
+        </div>
+        {/* search */}
+        
         <table className="membersTable">
+
         <tr>
           <th>Matrikel Nummer</th>
           <th>Name</th>
