f0d9e8e122f2c539a7b350d5f25da57554ce6029
andthengoto10 <andthengoto10@gmail.com>
2019-12-03 11:41:46 +0100

    Updates

---
+++ precache-manifest.e8cc7e062c710868b5e9984e4e700a16.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81853299a64e73fc17f6cd10467e4eb0",
+    "url": "/Final-project/index.html"
+  },
+  {
+    "revision": "1c02c0a856703c79bf8f",
+    "url": "/Final-project/static/css/main.4cae560a.chunk.css"
+  },
+  {
+    "revision": "c008df1ed7e1a6d6693e",
+    "url": "/Final-project/static/js/2.6c0d1d4f.chunk.js"
+  },
+  {
+    "revision": "1c02c0a856703c79bf8f",
+    "url": "/Final-project/static/js/main.e205252f.chunk.js"
+  },
+  {
+    "revision": "307c9d0c527441f009b5",
+    "url": "/Final-project/static/js/runtime-main.6136ee8e.js"
+  },
+  {
+    "revision": "9fa7c94eb8e141a3e17d2bb1b9ce93ff",
+    "url": "/Final-project/static/media/anton.9fa7c94e.png"
+  },
+  {
+    "revision": "3740cd1f2bed75dc1fb092cbb3f411b3",
+    "url": "/Final-project/static/media/bull.3740cd1f.png"
+  },
+  {
+    "revision": "12e55af3ba763a9a94d5770f49360917",
+    "url": "/Final-project/static/media/disappointment.12e55af3.jpg"
+  },
+  {
+    "revision": "7c27ffed1c9fd611fc0a973bd2433e24",
+    "url": "/Final-project/static/media/happy.7c27ffed.png"
+  },
+  {
+    "revision": "a904fed13785080428bcdf542926d11a",
+    "url": "/Final-project/static/media/horse.a904fed1.png"
+  },
+  {
+    "revision": "9eaab74dc857322ff99bbd9e4e549b71",
+    "url": "/Final-project/static/media/logo.9eaab74d.png"
+  },
+  {
+    "revision": "f6f796b7bd7ee01b5772cf1243b35e8a",
+    "url": "/Final-project/static/media/mad.f6f796b7.png"
+  },
+  {
+    "revision": "2e90aabc108d5e55038c9b79564caa6b",
+    "url": "/Final-project/static/media/monkey.2e90aabc.png"
+  },
+  {
+    "revision": "3dc92f5ba917145c4d76a49f2f4a4045",
+    "url": "/Final-project/static/media/mustafa.3dc92f5b.png"
+  },
+  {
+    "revision": "c596d6e843fdff4ac4ece680fb2b4368",
+    "url": "/Final-project/static/media/ontime.c596d6e8.jpg"
+  },
+  {
+    "revision": "67c377a2082234b3e2043625a47a718e",
+    "url": "/Final-project/static/media/penguin.67c377a2.png"
+  },
+  {
+    "revision": "731e056390891eeb689c6ff2c5d77ebb",
+    "url": "/Final-project/static/media/portfolioBg.731e0563.jpg"
+  },
+  {
+    "revision": "4903cec08a41919e51d9e449b8f28eef",
+    "url": "/Final-project/static/media/rabbit.4903cec0.png"
+  },
+  {
+    "revision": "1c78d7360133b8f718cc23db83252968",
+    "url": "/Final-project/static/media/rezan.1c78d736.png"
+  },
+  {
+    "revision": "8c0b4eb47f6e6c943fa8f2a24792f01d",
+    "url": "/Final-project/static/media/sad.8c0b4eb4.png"
+  },
+  {
+    "revision": "70e128ee343b01968d6a5f51d000be62",
+    "url": "/Final-project/static/media/tolate.70e128ee.jpg"
+  },
+  {
+    "revision": "ec94034af9b15b4eb350404a4457e03d",
+    "url": "/Final-project/static/media/waeel.ec94034a.png"
+  }
+]);
