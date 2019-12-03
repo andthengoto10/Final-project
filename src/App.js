8e3505af1b98371ea7638b546aba1b2c193eb06f
waeel-alsydo <waeel.alsydo@gmail.com>
2019-12-03 11:26:23 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Logo from "./assest/Image/logo.png";
 
 import Content from "./Components/Content/Content";
 
 function App() {
   return (
     <div>
+      <img src={Logo} alt="logo" className="logo" />
       <Content />
     </div>
   );
